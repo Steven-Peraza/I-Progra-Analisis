5137739cee77ca177d4ec1d11a6c95cefc9654e5
juanra027 <juanra_vm@hotmail.com>
2017-03-17 18:30:11 -0600

    prueba base

--- I Progra Analisis/I Progra Analisis/Pieza.cs
+++ I Progra Analisis/I Progra Analisis/Pieza.cs
@@ -8,6 +8,36 @@ namespace I_Progra_Analisis
 {
     class Pieza
     {
-        private String asd = "asdjkajd";
+        private int _numero;
+        private int[] _lados;
+
+        public Pieza()
+        {
+            this._numero = 0;
+            this._lados = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                this._lados[i] = 0;
+            }
+        }
+
+        public void setNumero(int numero)
+        {
+            this._numero = numero;
+        }
+        public int getNumero()
+        {
+            return this._numero;
+        }
+
+        public void setLado(int lado, int valor)//Si se quisiera poner un lado de la pieza manualmente
+        {
+            this._lados[lado] = valor;
+        }
+        public int getLado(int lado)
+        {
+            return this._lados[lado];
+        }
     }
+
 }
---
+++ I Progra Analisis/I Progra Analisis/Tetravex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Progra_Analisis
+{
+    class Tetravex
+    {
+        private Pieza[,] _tablero;
+        private int _tamaño;
+
+        public Tetravex(int tamaño)
+        {
+            this._tamaño = tamaño;
+            this._tablero = new Pieza[tamaño, tamaño];
+        }
+
+        public void setPieza(int fila, int columna, Pieza pieza)
+        {
+            this._tablero[fila, columna] = pieza;
+        }
+        public Pieza getPieza(int fila, int columna)
+        {
+            return this._tablero[fila, columna];
+        }
+    }
+}
